865592c75cecaa6b63aa9ce59d1276c1f89dee18
Madhumita SM <your_email@example.com>
2026-05-27 13:54:28 +0530

    connect frontend backend

--- cafe-app/frontend/vite.config.js
+++ cafe-app/frontend/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: { proxy: { '/api': 'http://localhost:5000', '/uploads': 'http://localhost:5000' } }
-})
+server: { 
+  proxy: { 
+    '/api': 'https://cafe-order-system-stl6.onrender.com', 
+    '/uploads': 'https://cafe-order-system-stl6.onrender.com' 
+  } 
+}
