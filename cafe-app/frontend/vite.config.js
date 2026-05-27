82bca71d77736f2937ee846c035159b5b62ec0e9
Madhumita SM <your_email@example.com>
2026-05-27 14:30:10 +0530

    fix vite config

--- cafe-app/frontend/vite.config.js
+++ cafe-app/frontend/vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-server: { 
-  proxy: { 
-    '/api': 'https://cafe-order-system-stl6.onrender.com', 
-    '/uploads': 'https://cafe-order-system-stl6.onrender.com' 
-  } 
-}
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'https://cafe-order-system-stl6.onrender.com',
+      '/uploads': 'https://cafe-order-system-stl6.onrender.com'
+    }
+  }
+})
